cb50cc55679d8472244b72877de56b1a1cc3bb0d
unknown <sarthak95063@gmail.com>
2019-12-20 16:52:57 +0530

    Database Updated again2

--- model/project/index.js
+++ model/project/index.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose')
+const URI = "mongodb://sarthak:sarthak12@ds249017.mlab.com:49017/notepad";
 mongoose.set('debug', true);
+mongoose.Promise = global.Promise
 
-mongoose.connect("mongodb://sarthak:sarthak12@ds249017.mlab.com:49017/notepad",{ useNewUrlParser: true });
 
+mongoose.connect(URI,{ useNewUrlParser: true  },(err)=>{
+    if (err){
+        console.log(err)
+    }else{
+        console.log("Connection Successful")
+    }
+});
 
 
 
