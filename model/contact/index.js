594cf870e4949fd15ea4eaa168f7eac13480a4a7
unknown <sarthak95063@gmail.com>
2019-12-20 16:22:38 +0530

    Database Updated

--- model/contact/index.js
+++ model/contact/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 mongoose.set('debug', true);
 
-mongoose.connect("mongodb://localhost/contact",{ useNewUrlParser: true ,useUnifiedTopology: true });
+mongoose.connect("mongodb://sarthak:sarthak12@ds249017.mlab.com:49017/notepad",{ useNewUrlParser: true ,useUnifiedTopology: true });
 
 
 mongoose.Promise = Promise;
